553e01099884a1b8339d0012c8787d67586d0fec
Boyani <sylviaboyani11@gmail.com>
2023-06-27 21:38:19 +0000

    done with hero section of raila

--- src/routes/churches/Nairobi/Raila/HomeR.js
+++ src/routes/churches/Nairobi/Raila/HomeR.js
@@ -1,16 +1,48 @@
-import React from 'react'
-import hey from "../../../../assets/first.jpeg"
-import { HashLink as Link } from 'react-router-hash-link'
+import React, { useEffect } from 'react'
+import './raila.css'
+import ImageSlider from './dataR'
+import { useState } from 'react'
+//import hey from "../../../../assets/first.jpeg"
+//import { HashLink as Link } from 'react-router-hash-link'
 
-function HomeR() {
+const HomeR = () => {
+  const [currentState,setCurrentState] = useState(0)
+  useEffect(() =>{
+    const timer = setTimeout(()=>{
+      if(currentState===3){
+        setCurrentState(0)
+      }else{
+        setCurrentState(currentState+1)
+      }
+    }, 5000)
+    return ()=> clearTimeout(timer)
+  },[currentState])
+  const bgImageStyle = {
+    backgroundImage: `url(${ImageSlider[currentState].url})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'contain',
+    height: '100%'
+  }
+  const goToNext = (currentState)=>{
+    setCurrentState(currentState)
+  }
   return (
-    <div>
-        <img className='heroImg' alt='heroImg' src={hey} />
-        <div className='hero-text'>
-          <h1>Come Let's Worship Together</h1>
-          <p>Welcome home, Karibu Nyumabani!</p>
-          <button><Link to='programs' />Let's talk</button>
-        </div>
+    <div className='hero'>
+       <div style={bgImageStyle} ></div>
+       <div className='transparent-background'></div>
+       <div className='description'>
+         <div>
+          <h1>{ImageSlider[currentState].title}</h1>
+          <p>{ImageSlider[currentState].body}</p>
+         </div>
+         <div className='carousel-boult'>
+          {
+            ImageSlider.map((ImageSlider,currentState)=>(
+              <span key={currentState} onClick={()=> goToNext(currentState) }></span>
+            ))
+          }
+         </div>
+       </div>
     </div>
   )
 }
--- src/routes/churches/Nairobi/Raila/dataR.js
+++ src/routes/churches/Nairobi/Raila/dataR.js
@@ -1,23 +1,25 @@
 const ImageSlider =[
     {
-        url: "Pic-1",
+        url: 'https://images.unsplash.com/photo-1533000971552-6a962ff0b9f9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80',
         title: "Men ministry doing their men stuff",
         body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut saepe voluptatum earum delectusdeserunt id iste, quas officiis et repellat!"
     },
     {
-        url: "Pic-2",
+        url: 'https://images.unsplash.com/photo-1543525238-54e3d131f7ca?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
         title: "Men ministry doing their men stuff",
         body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut saepe voluptatum earum delectusdeserunt id iste, quas officiis et repellat!"
     },
     {
-        url: "Pic-3",
+        url: 'https://images.unsplash.com/photo-1594100513700-659fa01ce822?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80',
         title: "Men ministry doing their men stuff",
         body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut saepe voluptatum earum delectusdeserunt id iste, quas officiis et repellat!"
     },
     {
-        url: "Pic-4",
+        url: 'https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
         title: "Men ministry doing their men stuff",
         body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut saepe voluptatum earum delectusdeserunt id iste, quas officiis et repellat!"
     },
     
 ]
+
+export default ImageSlider
