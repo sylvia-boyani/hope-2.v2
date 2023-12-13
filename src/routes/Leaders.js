92581934db8eadd02057f75a8f7fadd6fa9c88c6
Boyani <sylviaboyani11@gmail.com>
2023-12-13 10:28:45 +0000

    Minor edit on Leader page

--- src/routes/Leaders.js
+++ src/routes/Leaders.js
@@ -10,6 +10,7 @@ import im8 from '../assets/kelvin.jpeg'
 import im9 from '../assets/Nion.jpeg'
 import im10 from '../assets/Pst.Renson.jpeg'
 import im11 from '../assets/Eunice.jpeg'
+import im12 from '../assets/sly-sp.jpeg'
 import { BsTwitterX } from "react-icons/bs";
 import { BsFacebook, BsInstagram, BsLinkedin } from "react-icons/bs";
 
@@ -60,7 +61,9 @@ export default function Leaders() {
            <h6>Pastor James Mburu</h6>
            <div className='lead-info'>
             <p>Executive Board Vice Chairman</p>
-            <p>Fred is the current Bishop of Tumaini Ministries and the sole leader.</p>
+            <p>Pastor Mburu is the Lead Pastor of Tumaini Gitwamba. He is the Vice Chairman hence assisting the Bishop In administration.
+               He is also leader for other sub commitees.
+            </p>
               <ul className='lead-icons'>
               <li><a href="https://twitter.com" target="_blank" rel='nooopener noreferrer'><BsTwitterX/></a>
                  </li>
@@ -83,7 +86,7 @@ export default function Leaders() {
            <h6>Pastor James Wanyama</h6>
            <div className='lead-info'>
             <p>Executive Board Secretary</p>
-            <p>Fred is the current Bishop of Tumaini Ministries and the sole leader.</p>
+            <p>Pastor James is a pastor at Tumaini Raila. He is the general Secretary of the Board assisted with Margaret.</p>
               <ul className='lead-icons'>
               <li><a href="https://twitter.com" target="_blank" rel='nooopener noreferrer'><BsTwitterX/></a>
                  </li>
@@ -104,10 +107,12 @@ export default function Leaders() {
          <div className='col-sm-6 col-md-4'>
           <div className='lead-item'>
            <img className='lead-img'  src={im8} alt='pic' />
-           <h6>Fred Faradays Ogutu</h6>
+           <h6>Kelvin Otieno</h6>
            <div className='lead-info'>
-            <p>Executive Board Chairman</p>
-            <p>Fred is the current Bishop of Tumaini Ministries and the sole leader.</p>
+            <p>Executive Board Tresurer</p>
+            <p>Kelvin is a member and Worship Lead of Tumaini Gitwamba and the Ministry Treasurer. He
+               works hand in hand with Peter.
+            </p>
               <ul className='lead-icons'>
                  <li><a href="https://twitter.com" target="_blank" rel='nooopener noreferrer'><BsTwitterX/></a>
                  </li>
@@ -129,10 +134,10 @@ export default function Leaders() {
           <div className='col-sm-6 col-md-4'>
           <div className='lead-item'>
            <img className='lead-img'  src={im11} alt='pic' />
-           <h6>Pastor James Mburu</h6>
+           <h6>Eunice Atieno</h6>
            <div className='lead-info'>
-            <p>Executive Board Vice Chairman</p>
-            <p>Fred is the current Bishop of Tumaini Ministries and the sole leader.</p>
+            <p>Women's Ministry Lead</p>
+            <p>Eunice is a member of Tumaini Kayaba and the Women's Ministry Lead.</p>
               <ul className='lead-icons'>
               <li><a href="https://twitter.com" target="_blank" rel='nooopener noreferrer'><BsTwitterX/></a>
                  </li>
@@ -152,10 +157,11 @@ export default function Leaders() {
           <div className='col-sm-6 col-md-4'>
           <div className='lead-item'>
            <img className='lead-img'  src={im10} alt='pic' />
-           <h6>Pastor James Wanyama</h6>
+           <h6>Pastor Renson Mwembe</h6>
            <div className='lead-info'>
-            <p>Executive Board Secretary</p>
-            <p>Fred is the current Bishop of Tumaini Ministries and the sole leader.</p>
+            <p>Pastor's Representative</p>
+            <p>Pastor Renson is the Lead Pastor of Tumaini Kiambio and also the one representing all Pastors of Tumaini 
+               Ministry Churches. He is also the Chairman of the Pastors Council.</p>
               <ul className='lead-icons'>
               <li><a href="https://twitter.com" target="_blank" rel='nooopener noreferrer'><BsTwitterX/></a>
                  </li>
@@ -177,10 +183,12 @@ export default function Leaders() {
          <div className='col-sm-6 col-md-4'>
           <div className='lead-item'>
            <img className='lead-img'  src={im5} alt='pic' />
-           <h6>Fred Faradays Ogutu</h6>
+           <h6>Pastor Liz Pronk</h6>
            <div className='lead-info'>
-            <p>Executive Board Chairman</p>
-            <p>Fred is the current Bishop of Tumaini Ministries and the sole leader.</p>
+            <p>Children Ministry Lead</p>
+            <p>Pastor Liz is a member of Tumaini Grogon Church and also the children Ministry Lead, Leading all Children
+              (Sunday Schools) in all Tumaini Churches.
+            </p>
               <ul className='lead-icons'>
                  <li><a href="https://twitter.com" target="_blank" rel='nooopener noreferrer'><BsTwitterX/></a>
                  </li>
@@ -202,10 +210,12 @@ export default function Leaders() {
           <div className='col-sm-6 col-md-4'>
           <div className='lead-item'>
            <img className='lead-img'  src={im7} alt='pic' />
-           <h6>Pastor James Mburu</h6>
+           <h6>Pastor Peter Njasi</h6>
            <div className='lead-info'>
-            <p>Executive Board Vice Chairman</p>
-            <p>Fred is the current Bishop of Tumaini Ministries and the sole leader.</p>
+            <p>Ministry Accountant</p>
+            <p>Pastor Peter is the Ministry accountant and he has been serving in Tumaini Ministry.
+      
+            </p>
               <ul className='lead-icons'>
               <li><a href="https://twitter.com" target="_blank" rel='nooopener noreferrer'><BsTwitterX/></a>
                  </li>
@@ -225,10 +235,10 @@ export default function Leaders() {
           <div className='col-sm-6 col-md-4'>
           <div className='lead-item'>
            <img className='lead-img'  src={im6} alt='pic' />
-           <h6>Pastor James Wanyama</h6>
+           <h6>Margaret Wanjiku</h6>
            <div className='lead-info'>
-            <p>Executive Board Secretary</p>
-            <p>Fred is the current Bishop of Tumaini Ministries and the sole leader.</p>
+            <p>Youth Lead & Ass. Secretary</p>
+            <p>Margaret is a Member of Tumaini Grogon, the Mother church(First Church).</p>
               <ul className='lead-icons'>
               <li><a href="https://twitter.com" target="_blank" rel='nooopener noreferrer'><BsTwitterX/></a>
                  </li>
@@ -249,10 +259,12 @@ export default function Leaders() {
          <div className='col-sm-6 col-md-4'>
           <div className='lead-item'>
            <img className='lead-img'  src={im9} alt='pic' />
-           <h6>Fred Faradays Ogutu</h6>
+           <h6>Nixon Mambo</h6>
            <div className='lead-info'>
-            <p>Executive Board Chairman</p>
-            <p>Fred is the current Bishop of Tumaini Ministries and the sole leader.</p>
+            <p>Men's Ministry Lead</p>
+            <p>Nixon Mambo is a member of Tumaini Fuata Nyayo church. He leads the Ministry of men incoorparting all men 
+               from all Tumaini Churches.
+            </p>
               <ul className='lead-icons'>
                  <li><a href="https://twitter.com" target="_blank" rel='nooopener noreferrer'><BsTwitterX/></a>
                  </li>
@@ -273,11 +285,13 @@ export default function Leaders() {
 
           <div className='col-sm-6 col-md-4'>
           <div className='lead-item'>
-           <img className='lead-img'  src={im3} alt='pic' />
-           <h6>Pastor James Mburu</h6>
+           <img className='lead-img'  src={im12} alt='pic' />
+           <h6>Sylvia Boyani Otieno</h6>
            <div className='lead-info'>
-            <p>Executive Board Vice Chairman</p>
-            <p>Fred is the current Bishop of Tumaini Ministries and the sole leader.</p>
+            <p>Communications & Media Personell</p>
+            <p>Sylvia is the website developer and our main administrator of the website. She also handles all the
+               minitry's Communications and media and all IT related matters.
+            </p>
               <ul className='lead-icons'>
               <li><a href="https://twitter.com" target="_blank" rel='nooopener noreferrer'><BsTwitterX/></a>
                  </li>
